e4085d5e0ffb7c34fba68a14a28af61fe3bd8aa4
Mayankyadav3980 <ymayank501@gmail.com>
2022-03-02 12:04:06 +0530

    updated url

---
+++ build/precache-manifest.31f0fdd840e41a9667610acc1fda5810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d2c9f905bd75a94a0cee0211a710a2",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "97fb5bced2b18aa10b22",
+    "url": "/Keeper-App/static/js/2.0bf124bf.chunk.js"
+  },
+  {
+    "revision": "0f937621fff3d005d144",
+    "url": "/Keeper-App/static/js/main.00f046c7.chunk.js"
+  },
+  {
+    "revision": "128fd620914684fbc76b",
+    "url": "/Keeper-App/static/js/runtime-main.113ea231.js"
+  },
+  {
+    "revision": "3852da7130df9ba890cd8e6175c9d5cb",
+    "url": "/Keeper-App/static/media/formImg2.3852da71.jpg"
+  }
+]);
